cc92aa13831584d531caf8272e8b9ee02f9b8403
oigama <mathemovic.mg@gmail.com>
2023-03-17 09:47:05 -0300

    Calculator

---
+++ app.js
@@ -0,0 +1,23 @@
+let screen = document.querySelector(".screen");
+let buttons = document.querySelectorAll(".btn");
+let equal = document.querySelector(".btn-equal");
+let clear = document.querySelector(".btn-clear");
+
+buttons.forEach((buttons) => {
+  buttons.addEventListener("click", function (e) {
+    let value = e.target.dataset.num;
+    screen.value += value;
+  });
+});
+
+equal.addEventListener("click", function (e) {
+  if (screen.value === "") {
+    screen.value = "";
+  } else {
+    let answer = eval(screen.value);
+    screen.value = answer;
+  }
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
+});
